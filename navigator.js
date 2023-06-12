d497cc3cf9e9878673c0d23feae1866b0c2f4681
franck403 <79372051+franck403@users.noreply.github.com>
2023-06-12 17:56:23 -0400

    dsdsdsdsdsdsdsdsds

--- navigator.js
+++ navigator.js
@@ -9,7 +9,14 @@ const requestor = document.getElementsByTagName("body")[0];
 function handleError(event) {
     document.getElementById("fullscreen").src = "icon/button/2carre.png"
 }
+function afullscreen() {
+    var elements = document.getElementsByClassName("iframe");
 
+    for (var i = 0; i < elements.length; i++) {
+        var s = elements[i].src;
+        elements[i].src = s
+    }
+}
 document.addEventListener("fullscreenerror", handleError);
 // or
 document.onfullscreenerror = handleError;
