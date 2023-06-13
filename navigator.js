da84877bcad343c6922334c90d7511a38049fab0
franck403 <79372051+franck403@users.noreply.github.com>
2023-06-13 11:37:30 -0400

    asdasdas

--- navigator.js
+++ navigator.js
@@ -40,7 +40,7 @@ function close_tab(id) {
 }
 function new_tab() {
     var id = String(Math.random()).split(".")[1]
-    var template = `<div id="b${id}" onclick="select_tab(b${id})"><button>New tab</button><img src="icon/button/traverser.png" id="nb${id}" onclick="close_tab(b${id})"></div>` 
+    var template = `<div id="b${id}" onclick="select_tab('b${id}')"><button>New tab</button><img src="icon/button/traverser.png" id="nb${id}" onclick="close_tab('b${id}')"></div>` 
     var iframe = `<iframe id="bb${id}"></iframe>`
     var before = document.getElementById("tabs").innerHTML
     document.getElementById("tabs").innerHTML = before + template
