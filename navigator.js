595ceadd16f346d308a90de79a1636233c00905a
franck403 <79372051+franck403@users.noreply.github.com>
2023-06-13 06:56:31 -0400

    kjnjbhjbhb

--- navigator.js
+++ navigator.js
@@ -37,10 +37,16 @@ function close_tab(id) {
 }
 function new_tab() {
     var id = Math.random()
-    var template = `<div id="b${id}"><button>New tab</button><img src="icon/button/traverser.png" id="nb${id}" onclick="close_tab(b${id})"></div>` 
+    var template = `<div id="b${id}" onclick="select_tab(b${id})"><button>New tab</button><img src="icon/button/traverser.png" id="nb${id}" onclick="close_tab(b${id})"></div>` 
     var iframe = `<iframe id="bb${id}"></iframe>`
     var before = document.getElementById("tabs").innerHTML
     document.getElementById("tabs").innerHTML = before + template
     var before = document.getElementById("iframe").innerHTML
     document.getElementById("iframe").innerHTML = before + iframe
+}
+
+function select_tab(id) {
+    var div = document.getElementById(id)
+    var img = document.getElementById("n" + id)
+    var iframe = document.getElementById("b" + id)
 }
