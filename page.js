6056f958d778d90544b06924003cb40ef7939aa4
franck403 <79372051+franck403@users.noreply.github.com>
2023-06-14 08:51:06 -0400

    add js and api

--- page.js
+++ page.js
@@ -4,16 +4,37 @@ function title(text,id) {
 var searchbar = parent.document.getElementById("searchbar1")
 searchbar.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
-        var research = document.getElementById("searchbar1").value
-        var reg = "https://"
-    }
+        var research = parent.document.getElementById("searchbar1").value
+        if (research.slice(0, 8) == "https://") {
+            var id = parent.document.getElementsByClassName("active")[0].id
+            parent.document.getElementById("tt" + id).innerHTML = research.slice(1,20);
+            parent.document.getElementById("b" + id).src = "https://roaring-griffin-499a65.netlify.app/api/load/1?url=" + research
+        } else if (research.slice(0, 7) == "http://") {
+            var id = parent.document.getElementsByClassName("active")[0].id
+            parent.document.getElementById("tt" + id).innerHTML = research.slice(1,20);
+            parent.document.getElementById("b" + id).src = "https://roaring-griffin-499a65.netlify.app/api/load/2?url=" + research
+        } else {
+            var id = parent.document.getElementsByClassName("active")[0].id
+            parent.document.getElementById("tt" + id).innerHTML = research.slice(1,20);
+            parent.document.getElementById("b" + id).src = "https://roaring-griffin-499a65.netlify.app/api/load/3?q=" + research
+        }    }
 });
 
 var searchbar1 = document.getElementById("searchbar")
 searchbar1.addEventListener("keyup", function(event) {
-    if (event.key === "Enter") {
-        var research = document.getElementById("searchbar1").value
-        var reg = "https://"
+    var research = document.getElementById("searchbar").value
+    if (research.slice(0, 8) == "https://") {
+        var id = parent.document.getElementsByClassName("active")[0].id
+        parent.document.getElementById("tt" + id).innerHTML = research.slice(1,20);
+        parent.document.getElementById("b" + id).src = "https://roaring-griffin-499a65.netlify.app/api/load/1?url=" + research
+    } else if (research.slice(0, 7) == "http://") {
+        var id = parent.document.getElementsByClassName("active")[0].id
+        parent.document.getElementById("tt" + id).innerHTML = research.slice(1,20);
+        parent.document.getElementById("b" + id).src = "https://roaring-griffin-499a65.netlify.app/api/load/2?url=" + research
+    } else {
+        var id = parent.document.getElementsByClassName("active")[0].id
+        parent.document.getElementById("tt" + id).innerHTML = research.slice(1,20);
+        parent.document.getElementById("b" + id).src = "https://roaring-griffin-499a65.netlify.app/api/load/3?q=" + research
     }
 });
 
